eb334b8110b9098ddd165fe831bbe65faa0981c1
Angelvin69 <102989657+Angelvin69@users.noreply.github.com>
2024-10-12 18:59:13 -0600

    Update punteros.c

--- punteros.c
+++ punteros.c
@@ -6,5 +6,11 @@ int main()
     int x =10;
     int *p =&x;
     printf ("puntero %p \n",p);
+    
+
+    int y = 10;
+    int *p = *y;
+
+    printf("printo por referancia %p " , p)
     return 0;
 }
