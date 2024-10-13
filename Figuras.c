747b7762f3e7167198bee9c2d7ea322ceae1b693
Angelvin69 <102989657+Angelvin69@users.noreply.github.com>
2024-10-12 20:53:34 -0600

    Add files via upload

---
+++ Figuras.c
@@ -0,0 +1,45 @@
+void graficar_figura(int id) {
+    
+
+    case TRAPECIO:
+        int baseMayor = (int)(figura.dimension1 * escala);
+        int baseMenor = (int)(figura.dimension2 * escala);
+        int altura = (int)(figura.dimension3 * escala);
+
+        
+        float pendienteIzq = (float)(baseMayor - baseMenor) / (2 * altura);
+        float pendienteDer = -pendienteIzq;
+
+        
+        for (int y = ALTO / 2 - altura; y <= ALTO / 2; y++) {
+            
+            int inicioX = ANCHO / 2 - (baseMayor - (y - (ALTO / 2 - altura)) * pendienteIzq) / 2;
+            int finX = ANCHO / 2 + (baseMenor + (y - (ALTO / 2 - altura)) * pendienteDer) / 2;
+
+            
+            for (int x = inicioX; x <= finX; x++) {
+                canvas[y][x] = '*';
+            }
+        }
+        break;
+
+    }
+    void graficar_figura(int id) {
+    // ... (resto de la función sin cambios)
+
+    case ELIPSE:
+        int a = (int)(figura.dimension1 * escala); // Semieje mayor
+        int b = (int)(figura.dimension2 * escala); // Semieje menor
+
+        for (int y = -b; y <= b; y++) {
+            for (int x = -a; x <= a; x++) {
+                if ((x * x) / (a * a) + (y * y) / (b * b) <= 1) {
+                    canvas[ALTO / 2 + y][ANCHO / 2 + x] = '*';
+                }
+            }
+        }
+        break;
+    }
+
+    // ... (resto de la función sin cambios)
+}
